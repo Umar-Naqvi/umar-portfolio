75da8f4e987414c791d8fbeaff2c0640e6e3b47e
Mohammed Umar Ben Naqvi <mdumarnaqvi4@gmail.com>
2026-01-24 15:24:56 +0000

    use the background image with the same gausian blur on it form the publi

--- src/components/FluidBackground.tsx
+++ src/components/FluidBackground.tsx
@@ -1,10 +1,16 @@
 'use client';
 
+import Image from 'next/image';
+
 export default function FluidBackground() {
   return (
     <div className="fixed inset-0 -z-10 overflow-hidden bg-[#050505]">
-      <div className="absolute left-0 top-0 h-[40rem] w-[40rem] -translate-x-1/2 -translate-y-1/2 rounded-full bg-cyan-500/10 blur-[150px]" />
-      <div className="absolute bottom-0 right-0 h-[40rem] w-[40rem] translate-x-1/4 translate-y-1/4 rounded-full bg-purple-500/10 blur-[150px]" />
+      <Image
+        src="/bg.png"
+        alt="Abstract background"
+        fill
+        className="object-cover blur-3xl"
+      />
     </div>
   );
 }
