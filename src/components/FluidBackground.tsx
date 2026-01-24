73865c2f51e2374aa2bcb009bbf38112350e632d
Mohammed Umar Ben Naqvi <mdumarnaqvi4@gmail.com>
2026-01-24 15:37:42 +0000

    make it 15% opacity

--- src/components/FluidBackground.tsx
+++ src/components/FluidBackground.tsx
@@ -9,7 +9,7 @@ export default function FluidBackground() {
         src="/bg.png"
         alt="Abstract background"
         fill
-        className="object-cover blur-md opacity-[.15]"
+        className="object-cover blur-sm opacity-[.15]"
       />
     </div>
   );
